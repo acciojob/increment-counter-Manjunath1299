e0e0be052d5fa32f75d187c749503e65618a101d
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-12-20 17:54:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let count = 0;
-let counterElement = document.getElementById("counter");  // Get the paragraph element
+let counterElement = document.getElementById("counter");  
 let incrementBtn = document.getElementById("incrementBtn");
 
 incrementBtn.addEventListener("click", () => {
-  counterElement.innerText = count;  // Display the un-incremented value
-  alert(count);  // Alert the un-incremented value
-  count++;  // Increment the counter after displaying and alerting
+  counterElement.innerText = count;  
+  alert(count);  
+  count++;  
 });
