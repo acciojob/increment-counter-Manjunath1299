72502062e92eda960040fc64dc6c97ae6d9fd93b
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-12-20 18:01:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ let counterElement = document.getElementById("counter");
 let incrementBtn = document.getElementById("incrementBtn");
 
 incrementBtn.addEventListener("click", () => {
-	alert(count);  
+	// alert(count);  
   counterElement.innerText = count;  
   // alert(count);  
   count++;  
+	alert(count);
 });
